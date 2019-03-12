2e6a05e6350a7daadf6198298a8156a502ff97ef
Sandeep <46818993+TathvamSandeep@users.noreply.github.com>
2019-03-13 00:24:21 +0530

    Fibonacci Changes Done

--- myjavaproj/src/javapackage/Java02fibonacci.java
+++ myjavaproj/src/javapackage/Java02fibonacci.java
@@ -7,7 +7,7 @@ public class Java02fibonacci
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a Limit");
+		System.out.println("Enter a Limit number");
 		int l = sc.nextInt();
 		int x =0;
 		int y=1;
